0b0ae79cfde53f55756343f9bd598c59cac40809
jlhb1984 <jlhb1984@gmail.com>
2024-11-16 11:52:02 -0500

    Yield.

---
+++ generators.py
@@ -0,0 +1,16 @@
+def my_generator():
+    yield 1
+    yield 2
+    yield 3
+
+for value in my_generator():
+    print(value)
+
+def fibonacci(limit):
+    a,b=0,1
+    while a<limit:
+        yield a
+        a,b=b,a+b
+
+for num in fibonacci(10):
+    print(num)
